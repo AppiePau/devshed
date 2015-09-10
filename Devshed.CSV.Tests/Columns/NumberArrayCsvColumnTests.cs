21fbadf809fa1c489a7d5a34e6b3e8bcfbf9fb25
AppiePau <paul@devshed.nl>
2015-09-10 09:59:48 +0200

    Added missing files.

---
+++ Devshed.CSV.Tests/Columns/NumberArrayCsvColumnTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Devshed.Csv.Tests
+{
+    using System.Collections.Generic;
+    using Devshed.Shared;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+    using System.Text;
+    using System.Linq;
+
+    [TestClass]
+    public class NumberArrayCsvColumnTests
+    {
+        [TestMethod]
+        public void Read_NumberArray_ReadsNumbersFromQuotedString()
+        {
+            var definition = this.GetNumberDefinition();
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("\"Name\";\"Numbers\"\r\n"
+                            + "\"OK_NAME\";\"1,2,3\"\r\n"));
+
+            var rows = CsvReader.Read<NumberArrayModel>(stream, definition);
+            var row = rows.Single();
+            var testColors = row.Numbers.ToArray();
+
+            Assert.AreEqual("OK_NAME", row.Name);
+            Assert.AreEqual(1, testColors[0]);
+            Assert.AreEqual(2, testColors[1]);
+            Assert.AreEqual(3, testColors[2]);
+        }
+        
+        private CsvDefinition<NumberArrayModel> GetNumberDefinition()
+        {
+            return new CsvDefinition<NumberArrayModel>(
+                                new TextCsvColumn<NumberArrayModel>(e => e.Name),
+                                new ArrayCsvColumn<NumberArrayModel, int>(e => e.Numbers))
+            {
+                FirstRowContainsHeaders = true,
+                WriteBitOrderMarker = false
+            };
+        }
+        
+        private sealed class NumberArrayModel
+        {
+            public string Name { get; set; }
+
+            public int[] Numbers { get; set; }
+        }
+    }
+}
