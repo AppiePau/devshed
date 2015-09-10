4940c74e398a59a4edcfc733325ca0faa6675589
AppiePau <paul@devshed.nl>
2015-09-10 11:35:34 +0200

    Added Stream overload to GetImage() method.

---
+++ Devshed.Imaging.TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Devshed.Shared;
+using Devshed.Imaging;
+
+
+namespace Devshed.Imaging.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var file = File.OpenRead("E:\\Framed mat black.1.jpg"))
+            {
+                using (var output = new MemoryStream())
+                {
+                    Imaging.SaveImageTo(file, output, 200, 200, SizeMode.FitRatio);
+                }
+            }
+
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+        }
+    }
+}
