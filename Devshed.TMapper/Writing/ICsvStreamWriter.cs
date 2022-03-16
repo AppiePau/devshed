4b6fe81dda18fde0950c1bbcea1c48117b7d1b61
Paul Appeldoorn <paul@devshed.nl>
2022-03-16 09:07:15 +0100

    Cleanup, TMapper was an expiriment

--- Devshed.TMapper/Writing/ICsvStreamWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Devshed.Csv.Writing
-{
-    public interface ICsvStreamWriter
-    {
-        void Write<T>(Stream stream, T[] rows, CsvDefinition<T> definition);
-    }
-}
